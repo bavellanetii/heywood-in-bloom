dc27712c715ebad313872629c3bb5a630b62bb7f
Brandon Avellanet <bavellanetii@gmail.com>
2023-09-22 20:36:23 +0100

    added dbcontext

--- HeywoodInBloom/Data/ApplicationDbContext.cs
+++ HeywoodInBloom/Data/ApplicationDbContext.cs
@@ -1,6 +1,17 @@
-﻿namespace HeywoodInBloom.Data
+﻿using HeywoodInBloom.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HeywoodInBloom.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Sponsor> Sponsors { get; set; }
+        public DbSet<TeamMember> TeamMembers { get; set; }
     }
 }
